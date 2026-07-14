340cc2dbfc561804a8ac60a22443a1857131c198
AyaBarhoush <166550411+AyaBarhoush@users.noreply.github.com>
2026-07-14 16:52:28 +0300

    Delete tests/firstTest.spec.js

--- tests/firstTest.spec.js
+++
@@ -1,8 +0,0 @@
-import {test} from "@playwright/test"
-
-test ("Open ITG Website" , async ({page}) => {
-    await page.goto("https://itgsoftware.com/")
-    console.log("Website Opened Successfully ...! ")
-
-    await page.waitForTimeout(5000)
-}) 
